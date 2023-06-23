3139b1bda89e6902dc08912cc76d5a0a91e73015
jthatcher@synack.com <jthatcher@synack.com>
2023-06-23 18:57:12 +0100

    feat: optimal solution

--- rust/two_sum/src/main.rs
+++ rust/two_sum/src/main.rs
@@ -16,6 +16,21 @@ impl Solution {
         }
         vec![]
     }
+
+    // Optimal solution
+    pub fn two_sum_optimal(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        use std::collections::HashMap;
+        let mut map: HashMap<i32, i32> = HashMap::new();
+
+        for (idx, &num) in nums.iter().enumerate() {
+            if map.contains_key(&(target - num)) {
+                return vec![*map.get(&(target - num)).unwrap(), idx as i32];
+            } else {
+                map.insert(num, idx as i32);
+            }
+        }
+        vec![]
+    }
 }
 
 #[cfg(test)]
@@ -23,10 +38,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_two_sum() {
+    fn test_two_sum_brute() {
         assert_eq!(Solution::two_sum_brute(vec![2, 7, 11, 15], 9), vec![0, 1]);
         assert_eq!(Solution::two_sum_brute(vec![3, 2, 4], 6), vec![1, 2]);
         assert_eq!(Solution::two_sum_brute(vec![3, 3], 6), vec![0, 1]);
         assert_eq!(Solution::two_sum_brute(vec![3, 2, 4], 10), vec![]);
+    }
+
+    #[test]
+    fn test_two_sum_optimal() {
+        assert_eq!(Solution::two_sum_optimal(vec![2, 7, 11, 15], 9), vec![0, 1]);
+        assert_eq!(Solution::two_sum_optimal(vec![3, 2, 4], 6), vec![1, 2]);
+        assert_eq!(Solution::two_sum_optimal(vec![3, 3], 6), vec![0, 1]);
+        assert_eq!(Solution::two_sum_optimal(vec![3, 2, 4], 10), vec![]);
     }
 }
